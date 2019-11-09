9ef966b52f10d42e3d06ada74aa4ce99d941a8be
LuanaPorciuncula <lpb@cin.ufpe.br>
2019-11-09 17:01:20 -0300

    pontos e linhas no canvas

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-canvas.addEventListener("click", addPoint);
+var c = document.getElementById("myCanvas");
+c.addEventListener("click", addPoint);
+var ctx = c.getContext("2d");
+var points = Array();
+var count = 0;
+
+function addPoint(event) {
+    var x = event.clientX - c.getBoundingClientRect().left;
+    var y = event.clientY - c.getBoundingClientRect().top;
+    points.push([x, y]);
+    makePoint(x, y);
+    makeLine();
+}
+
+function makePoint(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    ctx.fillRect(x - 1.5, y - 1.5, 3, 3);
+    ctx.stroke();
+}
+
+function makeLine() {
+    if (count != 0) {
+        ctx.beginPath();
+        ctx.moveTo(points[count - 1][0], points[count - 1][1]);
+        ctx.lineTo(points[count][0], points[count][1]);
+        ctx.stroke();
+    }
+    count++;
+}
