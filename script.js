813597d307f5f4f3f99663885cf3b9fc28eb4dcc
Luana Porciuncula Barreto <lpb@WINDOWS.CIN.UFPE.BR>
2019-11-11 11:22:24 -0300

    Curva de bezier implementada

--- script.js
+++ script.js
@@ -2,29 +2,68 @@ var c = document.getElementById("myCanvas");
 c.addEventListener("click", addPoint);
 var ctx = c.getContext("2d");
 var points = Array();
-var count = 0;
+var pointsCurve = Array();
+var Curves = Array();
+var aval = 100;
 
 function addPoint(event) {
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
     var x = event.clientX - c.getBoundingClientRect().left;
     var y = event.clientY - c.getBoundingClientRect().top;
     points.push([x, y]);
-    makePoint(x, y);
-    makeLine();
+    if (points.length > 1) {
+        drawLines(points, "#FFD848");
+        pointsCurve = new Array();
+        makeCurve();
+    } else {
+        drawPoints();
+    }
 }
 
-function makePoint(x, y) {
-    ctx.beginPath();
-    ctx.arc(x, y, 5, 0, 2 * Math.PI);
-    ctx.fillRect(x - 1.5, y - 1.5, 3, 3);
-    ctx.stroke();
-}
-
-function makeLine() {
-    if (count != 0) {
+function drawPoints() {
+    ctx.strokeStyle = "#000000";
+    for(let i = 0; i < points.length;i++){
         ctx.beginPath();
-        ctx.moveTo(points[count - 1][0], points[count - 1][1]);
-        ctx.lineTo(points[count][0], points[count][1]);
+        ctx.arc(points[i][0], points[i][1], 5, 0, 2 * Math.PI);
+        ctx.fillRect(points[i][0] - 1.5, points[i][1] - 1.5, 3, 3);
         ctx.stroke();
     }
-    count++;
+}
+
+function drawLines(pointsSet, color) {
+    drawPoints();
+    ctx.strokeStyle = color;
+    for (let j = 0; j < pointsSet.length - 1; j++) {
+        ctx.beginPath();
+        ctx.moveTo(pointsSet[j][0], pointsSet[j][1]);
+        ctx.lineTo(pointsSet[j + 1][0], pointsSet[j + 1][1]);
+        ctx.stroke();
+    }
+}
+
+function makeCurve() {
+    let point;
+    for (let i = 0; i < aval; i++) {
+        point = castanhaDeCaju(points, i);
+        if (point)
+            pointsCurve.push(point);
+    }
+    pointsCurve.push(points[points.length-1]);
+    drawLines(pointsCurve,"#000000");
+}
+
+function castanhaDeCaju(anchorPoints, i) {
+    if (anchorPoints.length > 1) {
+        let point;
+        let aux = new Array();
+        for (let j = 0; j < anchorPoints.length - 1; j++) {
+            point = new Array([-1, -1]);
+            point[0] = anchorPoints[j][0] * (1 - (i / aval)) + anchorPoints[j + 1][0] * (i / aval);
+            point[1] = anchorPoints[j][1] * (1 - (i / aval)) + anchorPoints[j + 1][1] * (i / aval);
+            aux.push(point);
+        }
+        return castanhaDeCaju(aux, i);
+    } else {
+        return anchorPoints[0];
+    }
 }
